5f5ccf1d6cb12216b40266b7f3c99304bf72842a
naoki-yamanoi <ti44nnyy999@gmail.com>
2025-04-11 14:43:35 +0900

    ログイン修正、ログアウトapi作成

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class LoginController extends Controller
+class AuthController extends Controller
 {
     public function login(LoginRequest $request)
     {
         $validated = $request->validated();
 
         // 認証処理
-        if (Auth::attempt($validated)) {
+        if (Auth::attempt($validated))
+        {
             // 認証に成功した場合、APIトークンを発行
             $user = Auth::user();
             $token = $user->createToken('YourAppName')->plainTextToken;
@@ -24,5 +26,11 @@ class LoginController extends Controller
         }
 
         return response()->json(['message' => 'ログインに失敗しました。'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'ログアウトしました。']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
 
-Route::get('/user', function (Request $request) {
+Route::get('/user', function (Request $request)
+{
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/posts/follow', [PostController::class, 'getFollowPosts']);
+Route::middleware('auth:sanctum')->get('/posts/follow', [PostController::class, 'getFollowPosts']);
 
-Route::post('/login', [LoginController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
