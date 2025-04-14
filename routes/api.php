abf58ca1e71fa423c9087f23ab53054d1bd986e3
naoki-yamanoi <ti44nnyy999@gmail.com>
2025-04-14 15:00:52 +0900

    認証必要なルートをグループ化

--- routes/api.php
+++ routes/api.php
@@ -2,27 +2,24 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FollowController;
-use App\Http\Controllers\IndexController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, 'index']);
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::middleware('auth:sanctum')->get('/posts/follow', [PostController::class, 'getFollowPosts']);
-Route::middleware('auth:sanctum')->get('/posts/mine', [PostController::class, 'getMyPosts']);
-
-Route::middleware('auth:sanctum')->post('/post/create', [PostController::class, 'createPost']);
-
-Route::middleware('auth:sanctum')->post('/follow/create', [FollowController::class, 'createFollow']);
-Route::middleware('auth:sanctum')->post('/follow/delete', [FollowController::class, 'deleteFollow']);
-
-Route::middleware('auth:sanctum')->get('/users/recommend', [UserController::class, 'getRecommendUsers']);
-
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+// 認証必要なルート群
+Route::middleware('auth:sanctum')->group(function ()
+{
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/posts/follow', [PostController::class, 'getFollowPosts']);
+    Route::get('/posts/mine', [PostController::class, 'getMyPosts']);
+
+    Route::post('/post/create', [PostController::class, 'createPost']);
+
+    Route::post('/follow/create', [FollowController::class, 'createFollow']);
+    Route::post('/follow/delete', [FollowController::class, 'deleteFollow']);
+
+    Route::get('/users/recommend', [UserController::class, 'getRecommendUsers']);
+});
