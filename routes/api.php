60266e94d6a05000e47842234c896b8f2b5da885
naoki-yamanoi <ti44nnyy999@gmail.com>
2025-04-21 16:16:32 +0900

    テスト

--- routes/api.php
+++ routes/api.php
@@ -18,24 +18,24 @@ Route::post('/register', [UserController::class, 'registUser']);
 Route::post('/password/reset', [AuthController::class, 'resetPassword']);
 
 // 認証必要なルート群
-Route::group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
+// Route::middleware('auth:sanctum')->group(function () {
+Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::get('/posts/follow', [PostController::class, 'getFollowPosts']);
-    Route::get('/posts/like', [PostController::class, 'getLikePosts']);
-    Route::get('/posts/mine', [PostController::class, 'getMyPosts']);
-    Route::get('/posts/search', [PostController::class, 'getSearchPosts']);
+Route::get('/posts/follow', [PostController::class, 'getFollowPosts']);
+Route::get('/posts/like', [PostController::class, 'getLikePosts']);
+Route::get('/posts/mine', [PostController::class, 'getMyPosts']);
+Route::get('/posts/search', [PostController::class, 'getSearchPosts']);
 
-    Route::post('/post/create', [PostController::class, 'createPost']);
+Route::post('/post/create', [PostController::class, 'createPost']);
 
-    Route::post('/like/create', [LikeController::class, 'createLike']);
-    Route::post('/like/delete', [LikeController::class, 'deleteLike']);
+Route::post('/like/create', [LikeController::class, 'createLike']);
+Route::post('/like/delete', [LikeController::class, 'deleteLike']);
 
-    Route::post('/follow/create', [FollowController::class, 'createFollow']);
-    Route::post('/follow/delete', [FollowController::class, 'deleteFollow']);
+Route::post('/follow/create', [FollowController::class, 'createFollow']);
+Route::post('/follow/delete', [FollowController::class, 'deleteFollow']);
 
-    Route::get('/user', [UserController::class, 'getUserId']);
-    Route::post('/user/profile/edit', [UserController::class, 'editProfile']);
-    Route::get('/user/profile', [UserController::class, 'getUserProfile']);
-    Route::get('/users/recommend', [UserController::class, 'getRecommendUsers']);
-});
+Route::get('/user', [UserController::class, 'getUserId']);
+Route::post('/user/profile/edit', [UserController::class, 'editProfile']);
+Route::get('/user/profile', [UserController::class, 'getUserProfile']);
+Route::get('/users/recommend', [UserController::class, 'getRecommendUsers']);
+// });
